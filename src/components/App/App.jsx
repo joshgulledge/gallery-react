77fd8faeacc0b2f029963bc73278bf421acd5d8f
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-02-20 13:50:50 -0600

    like buttons and like counts work

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,13 +5,19 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [imageList, setImageList] = useState([]); //set as array
+
+  // this will get the previously loaded images on page load
+  useEffect(() => {
+    console.log('in the useEffect');
+    getImages();
+  }, []);
+
+
 
   // all my function declarations
-
   // this will get the data
   const getImages = function () {
-    console.log('in get images');
-
     // the axios "GET" call
     axios({
       method: 'GET',
@@ -26,15 +32,46 @@ function App() {
     }); // end axios get
   }; // end the getImages function
 
-  // this will get the previously loaded images on page load
-  useEffect(() => {
-    console.log('in the useEffect');
-    getImages();
-  }, []);
+
+
+  function ShowImages () {
+    // loop through and render on page
+    console.log('in show images: ', imageList);
+   return ( 
+     <div>
+    {imageList.map((img, index) => {
+      return (
+        <div key={img.id} className='pic-btn'>
+        <img className='pic-size' src= {img.path} />
+        <p>{img.likes} Likes</p>
+        <button id={img.id}className='like-btn' onClick={likeBtnClick}>LIKE</button>
+        </div>
+      )
+    })}
+    </div>
+   )
+  }
+
+  const likeBtnClick = function (e) {
+    let clickedPicId = e.target.id;
+    // this will 'PUT' an added like in data
+    axios({
+      method:'PUT',
+      url:`/gallery/like/${clickedPicId}`,
+    }).then(res => {
+      // re-render the picture
+      console.log('put res: ', res);
+      getImages();
+
+    }).catch(err => console.log(err)); // end put
+  } // end likeBtnClick
+
+
+
+  
 
   // this will be my variables and useStates
 
-  const [imageList, setImageList] = useState([]); //set as array
 
     return (
       <div className="App">
@@ -42,11 +79,10 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        {imageList.map(img => {
-          return (
-            <img key={img.id} className='pic-size' src= {img.path} />
-          )
-        })}
+
+        <ShowImages />
+
+        
         {/* <img src="images/goat_small.jpg"/> */}
       </div>
     );
